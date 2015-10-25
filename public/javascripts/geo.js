5abea5bd28a6f9360c57c4adbceea4ae9de4b953
GreatSalmon <ernestogramsch@gmail.com>
2015-10-25 10:33:28 -0400

    First commit
    Simple 2 user geotracking

---
+++ public/javascripts/geo.js
@@ -0,0 +1,80 @@
+var params = {};
+
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var username = params.userName;
+
+    var lat = $('#'+username).find('.lat')[0];
+    var lon = $('#'+username).find('.lon')[0];
+
+   // var lat = document.getElementById("lat");
+   // var lon = document.getElementById("lon");
+
+    lat.innerHTML = "Lat: " + position.coords.latitude; 
+    lon.innerHTML = "Lon: " + position.coords.longitude;
+}
+
+function showPositions(data){
+    
+    for (var i=0;i<data.length; i++){
+        var username = data[i].username;
+        var latDiv = $('#'+username).find('.lat')[0];
+        var lonDiv = $('#'+username).find('.lon')[0];
+        
+        latDiv.innerHTML = "Lat: " + data[i].lat; 
+        lonDiv.innerHTML = "Lon: " + data[i].lon;
+
+    }
+
+
+}
+
+function uploadPosition(position){
+	$.post("/updategeoloc",
+    {
+    	username: params.userName,
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
+    },
+        function(data) {
+            showPositions(data);
+        }
+    );
+}
+
+
+function geoerror(err) {
+		console.warn('ERROR(' + err.code + '): ' + err.message);
+        StartGeoLocation();
+}
+var options = {
+		enableHighAccuracy: false,
+		timeout: 0,
+		maximumAge: 0
+};
+//getLocation();
+
+function StartGeoLocation(){
+    var id = navigator.geolocation.watchPosition(uploadPosition, geoerror, options);
+
+}
+
+StartGeoLocation();
---
+++ routes/index.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var router = express.Router();
+
+function SendAllGeoLocsUpdated(req,res){
+    var db = req.db;
+    var collection = db.get('usercollection');
+
+    //var usersgeoloc = collection.find().lean();
+
+
+    collection.find({})
+        .on('success', 
+            function (doc) {
+                /* doc is the result available here */ 
+                res.json(doc);
+
+        });
+
+    // collection.find(function(err, users) {
+    //         if (err)
+    //             res.send(err);
+
+    //         res.json(users);
+    // });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('newuser', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist?userName="+userName);
+        }
+    });
+});
+
+/* POST to update geo location */
+router.post('/updategeoloc', function(req, res) {
+
+	// Set our internal DB variable
+    //console.log("hello");
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var lat = req.body.lat;
+    var lon = req.body.lon;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.update(
+    	{ "username" : userName },
+    	{
+    		$set: {"lat": lat, "lon": lon}
+    	},
+    	function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            SendAllGeoLocsUpdated(req,res);
+        }
+    }
+    );
+
+});
+
+module.exports = router;
